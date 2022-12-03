e64df3acdb8ea802941da2c01538c176b8a449ff
omar-su <73144494+Omar-su@users.noreply.github.com>
2022-12-03 13:01:02 +0100

    Infinite map is now implemented

---
+++ Assets/Script/LevelGenerator.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator: MonoBehaviour {
+    private const float PLAYER_DISTANCE_SPAWN_LEVEL_PART = 200f;
+    [SerializeField] private Transform levelPart_Start;
+    [SerializeField] private Transform levelPart_1;
+    private GameObject player;
+    private Vector3 lastEndPosition;
+    
+    private void Start() {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+    private void Awake() {
+        lastEndPosition = levelPart_Start. Find("LevelEnd").position;
+        int startingSpawnLevelParts = 5;
+        for (int i = 0; i < startingSpawnLevelParts; i++) {
+            SpawnLevelPart();
+        }
+    }
+    private void Update() {
+        if (Vector3.Distance(new Vector3(player.transform.position.x, player.transform.position.y), lastEndPosition) < PLAYER_DISTANCE_SPAWN_LEVEL_PART) {
+            // Spawn another level part
+            SpawnLevelPart();
+        }
+    }
+
+    private void SpawnLevelPart() {
+        Transform lastLevelPartTransform = SpawnLevelPart (lastEndPosition);
+        lastEndPosition = lastLevelPartTransform.Find("LevelEnd").position;
+    }
+
+    private Transform SpawnLevelPart (Vector3 spawnPosition) {
+        Transform levelPartTransform = Instantiate (levelPart_1, spawnPosition, Quaternion.identity);
+        return levelPartTransform;
+    }
+
+}
