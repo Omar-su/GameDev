2596b40ca95ce45c328a865e703b69f0d33500c0
isakswe <isak.gusten@telia.com>
2022-12-03 17:06:09 +0100

    Combined camera scripts

--- Assets/Script/CameraFollow.cs
+++ Assets/Script/CameraFollow.cs
@@ -6,13 +6,12 @@ public class CameraFollow : MonoBehaviour
 {
 
     public Transform followTransform;
-    
+    public GameObject player;
+    public float offsetSmoothing;
 
     // Update is called once per frame
-    void FixedUpdate()
-    {
-        this.transform.position = new Vector3(followTransform.position.x, followTransform.position.y, this.transform.position.z);
-        
-        
+    void FixedUpdate() {
+        Vector3 targetPostition = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, targetPostition, offsetSmoothing * Time.deltaTime);
     }
 }
