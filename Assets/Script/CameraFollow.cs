d84a5c708f259d2efcae2c1e49a42ef2980fba9c
omar-su <73144494+Omar-su@users.noreply.github.com>
2022-12-03 13:29:46 +0100

    Some changes

--- Assets/Script/CameraFollow.cs
+++ Assets/Script/CameraFollow.cs
@@ -2,17 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraFoloow : MonoBehaviour
+public class CameraFollow : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+    public Transform followTransform;
+    
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        this.transform.position = new Vector3(followTransform.position.x, followTransform.position.y, this.transform.position.z);
+        
         
     }
 }
